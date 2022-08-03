f9e16899c6e8d1faace9e88e58875daee432ce94
Aleksandra Herlender <aleksandra.herlender@gmail.com>
2022-08-03 22:16:18 +0200

    Dodałam moduł kodilla-intro oraz zadanie "Hello World"

---
+++ kodilla-intro/src/main/java/HalloWorld.java
@@ -0,0 +1,8 @@
+public class HalloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Aleksandra");
+        System.out.println("18");
+        System.out.println("M");
+    }
+}
