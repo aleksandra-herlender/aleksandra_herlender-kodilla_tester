ca07cbb2ec1c8d4cd3e24a6ef15b45fdc81e3174
Aleksandra Herlender <aleksandra.herlender@gmail.com>
2022-08-20 15:01:58 +0200

    zadanie kalkulator

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -4,10 +4,14 @@ public class Application {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
+
         int a = 5;
         int b = 8;
         int sumResult = calculator.sum(a, b);
+
+
         boolean correct = ResultChecker.assertEquals(13,sumResult);
+
         if (correct) {
             System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
         } else {
@@ -23,5 +27,14 @@ public class Application {
         } else {
             System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
         }
+
+        int squareResult = calculator.square(a);
+        boolean squareCorrect = ResultChecker.assertEquals(25,squareResult);
+        if (squareCorrect) {
+            System.out.println("Metoda square działa poprawnie dla liczby " + a );
+        } else {
+            System.out.println("Metoda square nie działa poprawnie dla liczby " + a );
+        }
+
     }
 }
