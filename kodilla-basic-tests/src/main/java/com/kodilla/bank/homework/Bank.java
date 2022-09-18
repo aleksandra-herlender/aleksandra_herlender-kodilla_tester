cb44e05197bf94cb039553a07c0d3803614f2fdb
Aleksandra Herlender <aleksandra.herlender@gmail.com>
2022-09-18 20:32:34 +0200

    3.8. Testy jednostkowe - ćwiczenia - prawdziwe implementacje

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -2,29 +2,57 @@ package com.kodilla.bank.homework;
 
 public class Bank {
 
-    private  CashMachine[] machines;
+    private CashMachine[] machines;
 
     public Bank(CashMachine[] machines) {
         this.machines = machines;
     }
 
-    public int totalBalance(){
-        return 0;
+    public int totalBalance() {
+        int balance = 0;
+        for (CashMachine cashMachine : this.machines) {
+            balance += cashMachine.balance();
+        }
+        return balance;
     }
 
-    public int withdrawalCount(){
-        return 0;
+    public int withdrawalCount() {
+        int count = 0;
+        for (CashMachine machine : this.machines) {
+            count += machine.withdrawalCount();
+        }
+        return count;
     }
 
-    public int incomesCount(){
-        return 0;
+    public int incomesCount() {
+        int count = 0;
+        for (CashMachine machine : this.machines) {
+            count += machine.incomesCount();
+        }
+        return count;
     }
 
-    public double averageWithdrawal(){
-        return  0.0;
+    public double averageWithdrawal() {
+        if (this.withdrawalCount() != 0) {
+            int sum = 0;
+            for (CashMachine machine : this.machines) {
+                sum += machine.totalWithdrawal();
+            }
+            return 1.0 * sum / this.withdrawalCount();
+        } else {
+            return 0.0;
+        }
     }
 
-    public double averageIncome(){
-        return  0.0;
+    public double averageIncome() {
+        if(this.incomesCount() != 0) {
+            int sum = 0;
+            for (CashMachine machine : this.machines) {
+                sum += machine.totalIncome();
+            }
+            return 1.0 * sum / this.incomesCount();
+        } else {
+            return 0.0;
+        }
     }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -3,18 +3,71 @@ package com.kodilla.bank.homework;
 public class CashMachine {
 
     private int[] transactions;
+    private int size;
 
-    public void addTransaction(int ammount){
+    public CashMachine() {
+        this.transactions = new int[0];
+        this.size = 0;
+    }
 
+    public void addTransaction(int amount){
+        if(amount != 0  && this.balance()+amount>=0) {
+            this.size++;
+            int[] newTab = new int[this.size];
+            System.arraycopy(transactions, 0, newTab, 0, transactions.length);
+            newTab[this.size - 1] = amount;
+            this.transactions = newTab;
+        }
     }
 
     public int balance(){
-        return 0;
+        int sum = 0;
+        for (int transaction : this.transactions) {
+            sum += transaction;
+        }
+        return sum;
     }
 
     public int transactionsCount(){
-        return 0;
+        return this.size;
     }
 
+    public int withdrawalCount(){
+        int count = 0;
+        for (int transaction: this.transactions) {
+            if(transaction<0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int incomesCount(){
+        int count = 0;
+        for (int transaction: this.transactions) {
+            if(transaction>0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public int totalWithdrawal(){
+        int total = 0;
+        for (int transaction : this.transactions) {
+            if(transaction<0) {
+                total += transaction;
+            }
+        }
+        return -1*total;
+    }
+    public int totalIncome(){
+        int total = 0;
+        for (int transaction : this.transactions) {
+            if(transaction>0) {
+                total += transaction;
+            }
+        }
+        return total;
+    }
 
 }
