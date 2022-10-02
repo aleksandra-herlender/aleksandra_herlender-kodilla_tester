ad8bb6fd6674b781a6c712b55f994916f11345be
Aleksandra Herlender <aleksandra.herlender@gmail.com>
2022-10-02 21:46:51 +0200

    9.2 Zadanie: Calculator

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimplyApplication.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.basic.spring_dependency_injection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class SimplyApplication {
+
+    private SkypeMessageService messageService;
+
+    public SimplyApplication(SkypeMessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public String processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
+            return this.messageService.send(message, receiver);
+        }
+        return null;
+    }
+
+    private boolean checkReceiver(String receiver) {
+        return receiver != null && !receiver.isEmpty();
+    }
+}
