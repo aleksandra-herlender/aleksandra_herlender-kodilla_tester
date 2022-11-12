754f52f63537952d761144e2dbf3decf312beab5
Aleksandra Herlender <aleksandra.herlender@gmail.com>
2022-11-12 22:06:39 +0100

    13.5 Zadanie: Ćwiczymy selektory CSS.

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,19 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");      // [1]
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
-        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/button[1]")).click();
-        WebElement category = driver.findElement(By.xpath("//*[@id=\"tab-dzialy\"]/div/div/div[1]/a"));
+        driver.findElement(By.cssSelector("#opbox-gdpr-consents-modal  button")).click();
+        WebElement category = driver.findElement(By.cssSelector("#tab-dzialy > div > div > div > a"));
         category.click();
 
-        WebElement input = driver.findElement(By.xpath("/html/body/div[2]/div[1]/header/div[1]/div/div/div/form/input"));
+        WebElement input = driver.findElement(By.cssSelector("form > input"));
         input.sendKeys("Mavic mini");
         input.submit();
-
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".opbox-listing article")));
+        List<WebElement> results = driver.findElements(By.cssSelector("section > article"));
+        for (WebElement result:results) {
+            System.out.println(result.getText());
+        }
     }
 }
